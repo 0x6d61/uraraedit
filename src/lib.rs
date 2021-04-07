38b375b884313a0f22a5e324538b4bcdd13f93da
0x6d61 <0x6d61@gmail.com>
2021-04-07 13:10:39 +0900

    下までスクロールできるように修正

--- src/lib.rs
+++ src/lib.rs
@@ -61,10 +61,13 @@ impl Editor {
         Terminal::cursor_position(&Position::default());
         if !self.should_quit {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y),
+            });
         }
         Terminal::cursor_show();
-        stdout().flush()
+        Terminal::flush()
     }
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = Terminal::read_key()?;
@@ -77,18 +80,71 @@ impl Editor {
 
         Ok(())
     }
+
+    fn scroll(&mut self) {
+        let Position {x,y} = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+
+        let mut offset = &mut self.offset;
+
+        if y < offset.y {
+            offset.y = y;
+        }else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+
+        if x < offset.x {
+            offset.x = x;
+        }else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
+    }
     fn move_cursor(&mut self, key: Key) {
+        let terminal_height = self.terminal.size().height as usize;
         let Position { mut x, mut y } = self.cursor_position;
         let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
+        let height = self.document.len();
+        let mut width = if let Some(row) = self.document.row(y) {
+            row.len()
+        }else{
+            0
+        };
         match key {
             Key::Up => y = y.saturating_sub(1),
             Key::Down => y = if y < height { y.saturating_add(1) } else { y },
-            Key::Left => x = x.saturating_sub(1),
-            Key::Right => x = if x < width { x.saturating_add(1) } else { x },
+            Key::Left => {
+                if x > 0 {
+                    x -= 1;
+                }else if y > 0 {
+                    y -= 1;
+                    if let Some(row) = self.document.row(y) {
+                        x = row.len();
+                    }else {
+                         x = 0;
+                    }
+                }
+            }
+            Key::Right => {
+                if x < width {
+                    x+=1;
+                }else if y < height {
+                    y += 1;
+                    x = 0;
+                }
+            }
             _ => (),
         }
+        width  = if let Some(row) = self.document.row(y) {
+            row.len()
+        }else{
+            0
+        };
+
+        if x > width {
+            x = width;
+        }
+
         self.cursor_position = Position { x, y }
     }
     pub fn draw_row(&self, row: &Row) {
@@ -100,7 +156,7 @@ impl Editor {
     }
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for terminal_row in 1..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
             if let Some(row) = self.document.row(terminal_row as usize + self.offset.y)  {
                 self.draw_row(row);
