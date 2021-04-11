ee22fca6281f2c372004e7e1aea8c5c600cbea46
0x6d61 <0x6d61@gmail.com>
2021-04-12 01:18:33 +0900

    document delete testを追加

--- tests/lib.rs
+++ tests/lib.rs
@@ -38,18 +38,20 @@ mod tests {
         let filename = "test_document_insert_newline.txt";
         create_example_file(&filename);
         let mut document = Document::open(&filename).unwrap();
-        //新しい行を追加
+        //行よりもyが大きかったら()を返す
         let position = Position{
             x:0,
             y:4,
         };
         assert_eq!(document.insert_newline(&position),());
+        //行数とyが一致したら行数を増やす。
         let position = Position{
             x:0,
             y:3,
         };
         document.insert_newline(&position);
         assert_eq!(document.rows.len(),4);
+        //文章の途中でEnter押した感じ
         let position = Position{
             x:4,
             y:0,
@@ -64,6 +66,55 @@ mod tests {
         let filename = "test_document_insert.txt";
         create_example_file(&filename);
         let mut document = Document::open(&filename).unwrap();
+        //最後の行で文字を追加したとき
+        let position = Position{
+            x:0,
+            y:3,
+        };
+        document.insert(&position,'x');
+        if let Some(row) = document.row(4) {
+            assert_eq!(row.string,'x'.to_string());
+        }
+        //
+        let position = Position{
+            x:1,
+            y:0,
+        };
+        document.insert(&position,'x');
+        if let Some(row) = document.row(4) {
+            assert_eq!(row.string,"txest".to_string());
+        }
+        remove_example_file(filename);
+    }
+    #[test]
+    fn test_document_delete() {
+        let filename = "test_document_delete.txt";
+        create_example_file(&filename);
+        let mut document = Document::open(&filename).unwrap();
+        let position = Position{
+            x:0,
+            y:4,
+        };
+        assert_eq!(document.delete(&position),());
+        let position = Position {
+            x:4,
+            y:0,
+        };
+        document.delete(&position);
+        let result = document.row(0);
+        if let Some(line) = result {
+            assert_eq!(line.string,"testfoo".to_string());
+        }
+        let position = Position {
+            x:1,
+            y:0,
+        };
+        document.delete(&position);
+        let result = document.row(0);
+        if let Some(line) = result {
+            assert_eq!(line.string,"tstfoo".to_string());
+        }
+        remove_example_file(filename);
 
     }
 }
